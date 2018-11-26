7c0ca2ec87d0dacd2c980f748410b47b964127a0
Lucas Michals <lucasmichals@Lucass-MBP.domain>
2018-11-25 20:24:22 -0800

    cleanup files

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
 import Hello from "./components/Hello";
 
 ReactDOM.render(
   <Hello name="TypeScript" enthusiasmLevel={10} />,
   document.getElementById("root") as HTMLElement
 );
-
-registerServiceWorker();
